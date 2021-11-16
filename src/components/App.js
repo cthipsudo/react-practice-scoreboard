614b1bfacaabe886772e6c61a1db543c22972663
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-11-15 18:22:38 -0600

    added player form and controlled value component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Header from './Header';
 import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 class App extends Component {
   state = {
@@ -29,8 +30,14 @@ class App extends Component {
     ]
   };
 
+  handleScoreChange = (index, delta) => {
+    this.setState(prevState => ({
+      score: prevState.players[index].score += delta
+    }));
+  }
+
   handleRemovePlayer = (id) => {
-    this.setState( prevState => {
+    this.setState(prevState => {
       return {
         players: prevState.players.filter(p => p.id !== id)
       };
@@ -40,21 +47,24 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header 
-          title="Scoreboard" 
-          totalPlayers={this.state.players.length} 
+        <Header
+          title="Scoreboard"
+          players= {this.state.players}
         />
-  
+
         {/* Players list */}
-        {this.state.players.map( player =>
-          <Player 
+        {this.state.players.map((player, index) =>
+          <Player
             name={player.name}
             score={player.score}
             id={player.id}
-            key={player.id.toString()} 
-            removePlayer={this.handleRemovePlayer}           
+            key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}
           />
         )}
+        <AddPlayerForm />
       </div>
     );
   }
